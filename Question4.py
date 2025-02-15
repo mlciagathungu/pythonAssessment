86afaa6dd82a7bb596b1b1f515e523d4023e351c
mlciagathungu <mlciagathungu1022@gmail.com>
2025-02-15 13:51:14 +0300

    Update Question4.py

--- Question4.py
+++ Question4.py
@@ -1,6 +1,6 @@
-a=47
-b=34
-c=21
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=int(input("Enter the third number:"))
 def get_max(a,b,c):
     if a>b and a>c:
         return a
