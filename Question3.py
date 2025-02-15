ebd3e33a7a4f6e320f8d3f050fc4ef2dcae2bc8b
mlciagathungu <mlciagathungu1022@gmail.com>
2025-02-15 14:19:27 +0300

    Update Question3.py

--- Question3.py
+++ Question3.py
@@ -1,10 +1,8 @@
-y=1
-while y<=10:
-    y += 1
-    print(y)
-for  3 in y:
-    print("Fizz")
-elif y/5:
-    print("Buzz")
-else:
-    print(y)
+for y in range(1, 11):
+    if y % 3 == 0:
+        print("Fizz")
+    elif y % 5 == 0:
+        print("Buzz")
+    else:
+        print(y)     
+    
